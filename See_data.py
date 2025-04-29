282d7ade3b63095d633ebcec76c8d81e7e6c938a
caden-calderon <caden.calderon03@gmail.com>
2025-04-29 15:57:03 -0600

    Setup + recording

---
+++ See_data.py
@@ -0,0 +1,7 @@
+# Author: Caden Calderon 
+
+import numpy as np
+
+seq = np.load("collected_data/assign_name/sequence_6.npy")  # <<< Adjust path here as needed 
+print(seq.shape)  # (Sequence length, 63)
+print(seq)        # See all the data 
